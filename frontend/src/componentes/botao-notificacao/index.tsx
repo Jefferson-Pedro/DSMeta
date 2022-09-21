ce0f310afcb80ada88820d5bfd73fcba5596b953
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-09-21 09:39:07 -0300

    Envio de Notificação
    Configuração do botão para envio de SMS

--- frontend/src/componentes/botao-notificacao/index.tsx
+++ frontend/src/componentes/botao-notificacao/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios'
 import icon from '../../assets/img/notificacao-icon.svg'
+import { BASE_URL } from '../../utils/request'
 import './stylesBotaoNofiticacao.css'
 
-function NotificacaoBotao() {
+type Props = {
+    saleId: number;
+}
+
+function handClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("Sucesso!");
+    });
+}
+
+function NotificacaoBotao({saleId}: Props) {
     return (
-        <div className="tabela-vendas-btn-red">
+        <div className="tabela-vendas-btn-red" onClick={() => handClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
